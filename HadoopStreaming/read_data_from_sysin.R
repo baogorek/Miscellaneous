46d0411cd2bc0680c278c221dad92d24e719d366
Ben Ogorek <baogorek@gmail.com>
2016-07-25 07:49:22 -0500

    added chunked read and write demo to read_data_from_sysin.R

--- HadoopStreaming/read_data_from_sysin.R
+++ HadoopStreaming/read_data_from_sysin.R
@@ -1,5 +1,14 @@
 
+
+cat("\nchunk 1\n\n")
+# chunk 1
 system_in <- file("stdin")
 open(system_in)
 data <- read.csv(system_in, nrows = 5)
-print(data)
+write.csv(data, file = stdout(), row.names = F, quote = F)
+
+cat("\n\nchunk 2\n\n")
+# chunk 2
+data <- read.csv(system_in, nrows = 5, header = F)
+write.table(data, file = stdout(), row.names = F, col.names = F, quote = F)
+
