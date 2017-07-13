195c4b4115ae192a39b6d6833ebf7686fe9e28e7
Ben Ogorek <baogorek@gmail.com>
2017-07-13 08:45:35 -0500

    last changes before removal

--- R-code/simulation/Simulating_One_Dim_Ito_Diffusions.R
+++ R-code/simulation/Simulating_One_Dim_Ito_Diffusions.R
@@ -1,16 +1,15 @@
- #equation for the dynamics of the price of a stock in the Black Scholes options pricing model of financial mathematics.
-# dXt = mu*Xt*dt + sigma*xt*dBt
+# The dynamics of the price of a stock in the Black Scholes options pricing model
 
 delta <- .01
-t <- seq(0,1, by = delta)
+t <- seq(0, 1, by = delta)
 T <- length(t)
 x <- rep(1, times = T)
 
 for(i in 2:T){
-  x[i] <- x[i-1] + (.1)*x[i-1]*.01 + (.5)*x[i-1]*rnorm(1, 0, .01)
+  x[i] <- x[i - 1] + .1 * x[i - 1] * delta + .5 * x[i - 1] * rnorm(1, 0, .01)
 }
 
- plot(x~t, type = "l")
+plot(x ~ t, type = "l")
 
 
 ### A linear feedback model, with goal G ###
@@ -25,7 +24,7 @@ r = 2.5
 x[0] <- 5
 
 for(i in 2:T){
-  x[i] <- x[i-1] + r*(G-x[i-1])*.01 + (1)*x[i-1]*rnorm(1, 0, .01)
+  x[i] <- x[i - 1] + r * (G - x[i - 1]) * delta + x[i - 1] * rnorm(1, 0, .01)
 }
 
  plot(x~t, type = "l")
