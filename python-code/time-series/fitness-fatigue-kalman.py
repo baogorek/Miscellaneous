6c660c0d4769a6e627478f464c5d156c8741c901
Ben Ogorek <baogorek@gmail.com>
2019-01-16 18:59:14 -0600

    working fitness fatigue model with kalman filter

--- python-code/time-series/fitness-fatigue-kalman.py
+++ python-code/time-series/fitness-fatigue-kalman.py
@@ -9,36 +9,52 @@ from statsmodels.tsa.statespace.mlemodel import MLEModel
 train_df = pd.read_csv("c:/devl/data/train_df.csv")
 
 class FitnessFatigue(MLEModel):
-    start_params = [30, 8, .07, .3, 400, 1.2] # Get same ones from R TODO
+    start_params = [60, 13, .07, -.27, 496, 49] # Get same ones from R TODO
     param_names = ['tau_1', 'tau_2', 'k1', 'k2', 'int', 'sigma2_e']
 
     def __init__(self, endog, intensities):
         super().__init__(endog, k_states = 2)
+        
+        self.initialize_known(np.array([0, 0]), np.eye(2))
 
         self.intensities = intensities
-        self.initialize_approximate_diffuse()
-        self.loglikelihood_burn = 1
+        #self.loglikelihood_burn = 1
 
     def update(self, params, **kwargs):
 
         ## state part
-        T = np.exp(-np.diag([params[0], params[1]]))
-
-        self.transition = T
-    
-        self.state_intercept = np.matmul(T, self.intensities) # i.e., Bu
+        elements = np.exp(np.array([-1.0 / params[0], -1.0 / params[1]]))
+        self['transition'] = np.diag(elements)
+        
+        self['state_intercept'] = np.matmul(np.reshape(elements, (2, 1)),
+                                            self.intensities)
         ## measurement part
-
-        # design is a row vector
         self['design', 0, 0] = params[2]
         self['design', 0, 1] = params[3]
 
-        self['obs_intercept'] = [params[4]]
+        self['obs_intercept', 0, 0] = params[4]
         self['obs_cov', 0, 0] = params[5]
 
+u = np.reshape(np.append(0, train_df.w.values[0:-1]), (1, train_df.shape[0]))
+my_FitnessFatigue = FitnessFatigue(train_df.perf.values, u)
+mle_results = my_FitnessFatigue.fit(method = 'nm', maxiter = 1000)
 
-u = np.reshape(np.r_[[train_df.w, train_df.w]], (2, train_df.shape[0]))
-my_FitnessFatigue = FitnessFatigue(train_df.perf, u)
-mle_results = my_FitnessFatigue.fit(method = 'bfgs')
+# You can really peak inside using the filter object
+my_filter = mle_results.filter_results # underlying model & KF output
+my_filter.shapes
+
+#plt.plot(my_filter.forecasts[0]) # One step ahead forecasts
+#plt.show()
+
+# Let's see if we can match the predictions:
+my_filter.forecasts[0][0]
+
+pr = my_filter.predict(0, 5, 0).results # starts dynamic predictions at 0
+
+pr.forecasts
+# Note the equivalence
+pr.filtered_state
+pr.predicted_state
+
 mle_results.summary()
 
