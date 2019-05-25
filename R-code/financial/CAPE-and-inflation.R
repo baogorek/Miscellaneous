83fd7611aaaeb76cbdebef0dba46f1f8ffc7c2fc
Ben Ogorek <baogorek@gmail.com>
2019-05-25 10:12:49 -0500

    Adding files

--- R-code/financial/CAPE-and-inflation.R
+++ R-code/financial/CAPE-and-inflation.R
@@ -1,7 +1,17 @@
+# Paper about cape:
+# https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3258404
+
+# Why am I getting this data from multipl.com when Shiller is updating it
+# himself? It's here: http://www.econ.yale.edu/~shiller/data.htm
+
+
+
 library(rvest)
 library(dplyr)
 library(ggplot2)
 library(ggthemes)
+library(lubridate)
+library(RcppRoll)
 
 # CAPE data ------------------------------------------------------------
 cape_url <- "http://www.multpl.com/shiller-pe/table"
@@ -46,16 +56,16 @@ inflation_table$inflation <- as.numeric(gsub("%", "",
                                            inflation_table$inflation))
 
 # Inflation adjusted S&P price data -----------------------------------
-prices_url <- "http://www.multpl.com/inflation-adjusted-s-p-500/table/by-year"
-table_list <- prices_url %>%
+adj_sp500_u <- "http://www.multpl.com/inflation-adjusted-s-p-500/table/by-year"
+table_list <- adj_sp500_u %>%
               read_html()  %>%
               html_nodes("table#datatable") %>%
               html_table()
 
-prices_table <- table_list[[1]]
-names(prices_table) <- c("date", "price")
-prices_table$date <- as.Date(prices_table$date, format = "%B %d, %Y")
-prices_table$price <- as.numeric(gsub(",", "", prices_table$price))
+adj_sp500_tbl <- table_list[[1]]
+names(adj_sp500_tbl) <- c("date", "adj_sp500")
+adj_sp500_tbl$date <- as.Date(adj_sp500_tbl$date, format = "%B %d, %Y")
+adj_sp500_tbl$adj_sp500 <- as.numeric(gsub(",", "", adj_sp500_tbl$adj_sp500))
 
 # Raw S&P Prices table
 raw_sp500_url <- "http://www.multpl.com/s-p-500-historical-prices/table/by-year"
@@ -80,29 +90,110 @@ table_list <- earnings_url %>%
 
 earnings_table <- table_list[[1]]
 earnings_table$Date <- as.Date(earnings_table$Date, format = "%B %d, %Y") + 1
-names(earnings_table) <- c("date", "earnings")
+names(earnings_table) <- c("date", "adj_earnings")
 # Adding 2018 final earnings estimate from:
 # https://us.spindices.com/documents/additional-material/sp-500-eps-est.xlsx
 earnings_table <- rbind(data.frame(date = as.Date("2019-01-01"),
-                                   earnings = 157.76), earnings_table)
+                                   adj_earnings = 157.76), earnings_table)
+
+# S&P Real Dividends
+dividends_url <- "http://www.multpl.com/s-p-500-dividend/table"
+table_list <- dividends_url %>%
+              read_html()  %>%
+              html_nodes("table#datatable") %>%
+              html_table()
+
+dividends_table <- table_list[[1]]
+# Note: The dividends are reported at end of year. +1 sends to next year
+dividends_table$Date <- as.Date(dividends_table$Date, format = "%B %d, %Y") + 1
+names(dividends_table) <- c("date", "lag_adj_div")
+
 # Data processing and analysis
 data_tbl <- cape_table %>%
   inner_join(t_rate_table) %>%
-  inner_join(prices_table) %>%
+  inner_join(adj_sp500_tbl) %>%
   inner_join(earnings_table) %>%
   inner_join(inflation_table) %>%
   inner_join(raw_sp500_table) %>%
-  mutate(adj_price_in_10yrs = lead(price, n = 10, order = date),
-         sp500_price_in_10yrs = lead(raw_sp500, n = 10, order = date)) %>%
-  arrange(desc(date))
+  inner_join(dividends_table) %>%
+  mutate(lag_adj_sp500 = lag(adj_sp500, n = 1, order_by = date)) %>%
+  arrange(date)
+
+data_tbl$year_return <- with(data_tbl,
+  (adj_sp500 + lag_adj_div) / lag_adj_sp500)
 
 data_tbl <- data_tbl %>%
-  mutate(real_ten_year_return = adj_price_in_10yrs / price - 1.0,
-         nom_ten_year_return = sp500_price_in_10yrs / raw_sp500 - 1, 
-         real_interest = treasury_rate10 - inflation
-)
+  mutate(invest_jan_of_year = year(date) - 1,
+         adj_10yr_ret = 100 * exp(roll_mean(log(year_return), n = 10,
+                                            na.rm = FALSE, align = "left",
+                                            fill = NA)) - 100,
+         adj_10yr_earn = roll_mean(adj_earnings, n = 10, na.rm = FALSE,
+                                   align = "right", fill = NA),
+         real_interest = treasury_rate10 - inflation) %>%
+  arrange(desc(date))
+data_tbl$cape2 <- data_tbl$adj_sp500 / data_tbl$adj_10yr_earn
 
-# TODO: reconcile the 300% number. Is that annualized?
+# Did a manual spot check for 3 period moving average. A todo would be to
+#  get this into a function and test against a known standard
+
+# Figure 1.1 Irrational Exhuberance
+ggplot(data_tbl, aes(x = year)) +
+  geom_line(aes(y = 5 * adj_earnings, color = "adj earnings x5")) +
+  geom_line(aes(y = adj_sp500, color = "adj sp500")) +
+  scale_x_continuous(name = "Year", limit = c(1860, 2020),
+                     breaks = seq(1860, 2020, 20)) +
+  scale_y_continuous(name = "Value", limit = c(0, 3000),
+                     breaks = seq(0, 3000, 200)) +
+  scale_color_manual(name = "Series",
+                     values = c("adj earnings x5" = "green",
+                                "adj sp500" = "red"))
+
+# Figure 1.2 IE
+ggplot(data_tbl, aes(x = year, y = cape)) +
+  geom_line() +
+  scale_x_continuous(name = "Year", limit = c(1860, 2020),
+                     breaks = seq(1860, 2020, 20)) +
+  scale_y_continuous(name = "Value", limit = c(0, 45),
+                     breaks = seq(0, 45, 5))
+
+# Figure 1.3 IE
+# Annualized 10-year returns
+  # For January of the year indicated
+  # Geometric average real annual return per year, reinvesting dividends
+# 18xx gets asterics
+cent_prefix <- ifelse(substr(data_tbl$invest_jan_of_year, 1, 2) == "18",
+                             "*", "")
+data_tbl$display_year <- paste0(cent_prefix,
+                                substr(data_tbl$invest_jan_of_year, 3, 4))
+
+n <- nrow(data_tbl)
+#TODO: figure out how I got off by one with CAPE measurements
+data_tbl$cape2 <- c(data_tbl$cape[-1], NA)
+
+ggplot(data_tbl, aes(x = cape2, y = adj_10yr_ret)) +
+  #geom_point() + keep alive just to calibrate it with hjust, vjust
+  geom_text(aes(label = display_year), hjust = .5, vjust = .35, size = 2.5) + 
+  scale_x_continuous(name = "Price-earnings", limit = c(5, 30),
+                     breaks = seq(5, 30, 5)) +
+  scale_y_continuous(name = "Ten year Return", limit = c(-5, 20),
+                     breaks = seq(-5, 20, 5))
+
+
+
+# Quick test for return calculation
+library(RcppRoll)
+test_tbl <- data.frame(year = c(1, 2, 3, 4), price = c(100, 120, 114, 116))
+test_tbl <- test_tbl %>%
+  mutate(rel_return = price / (lag(price, n = 1, order_by = year)))
+
+stopifnot(all(test_tbl$rel_return[2:3] == c(1.20, .95)))
+
+test_tbl <- test_tbl %>%
+  mutate(two_year_return = exp(roll_mean(log(rel_return), n = 2, na.rm = TRUE,
+                                     align = "right", fill = NA)))
+
+
+
 
 # What is going on? Need a better measure of short term real interest
 plot(real_interest ~ date, data = data_tbl)
--- R-code/financial/tr-cape.R
+++ R-code/financial/tr-cape.R
@@ -40,15 +40,19 @@ cape_yearly <- cape_monthly %>%
   filter(round(Date %% 1, 2) == .01) %>%
   mutate(snap_jan_yr = round(Date))
 
-cape_yearly <- cape_yearly %>%
-  mutate(display_year = paste0(ifelse(substr(snap_jan_yr, 1, 2) == "18",
-                                      "*", ""),
-                               substr(snap_jan_yr, 3, 4)))
-# %>%
-#  select(snap_jan_yr, display_year, cpi, interest_rate10, raw_sp500, real_price,
-#         real_earnings, cape, tr_cape, ten_yr_return, lag_tr_cape_1mo,
-#         lag_cape_1mo)
+add_century_symbol <- function(year) {
+  symbol <- ""
+  if(substr(year, 1, 2) == "18") {
+    symbol <- "*"
+  } else if (substr(year, 1, 2) == "20") {
+    symbol <- "\u2020"
+  }
+  paste0(symbol, substr(year, 3, 4))
+}
 
+cape_yearly$display_year <- sapply(1:nrow(cape_yearly),
+  function(i) add_century_symbol(cape_yearly$snap_jan_yr[i]))
+                       
 # Figure 1.1 Irrational Exhuberance
 ggplot(cape_yearly, aes(x = snap_jan_yr)) +
   geom_line(aes(y = 5 * real_earnings, color = "Real earnings x5")) +
@@ -90,7 +94,17 @@ ggplot(cape_yearly, aes(x = tr_cape, y = lead_10yr_ret)) +
 # For reference:
 sum(is.na(cape_yearly$cape) | is.na(cape_yearly$ten_yr_return))
 
+
 library(MASS)
+
+#cape_yearly2 <- cape_yearly
+cape_yearly <- cape_yearly2
+cape_yearly <- cape_yearly %>% filter(!(snap_jan_yr %in% c(1999, 2000, 2001)))
+cape_yearly <- cape_yearly %>%
+  add_row(display_year = "yy", tr_cape = 35, lead_10yr_ret = -10) %>%
+  add_row(display_year = "zz", tr_cape = 33, lead_10yr_ret = -10)
+
+
 my_rlm <- rlm(lead_10yr_ret ~ tr_cape, data = cape_yearly)
 my_lm <- lm(lead_10yr_ret ~ tr_cape, data = cape_yearly)
 
@@ -106,8 +120,8 @@ ggplot(cape_yearly, aes(x = tr_cape, y = lead_10yr_ret)) +
   geom_line(aes(y = pred_return_lm, color = "OLS Regression")) + 
   scale_x_continuous(name = "TR-CAPE", limit = c(5, 50),
                      breaks = seq(5, 50, 5)) +
-  scale_y_continuous(name = "Ten year Return", limit = c(-5, 20),
-                     breaks = seq(-5, 20, 5)) +
+  scale_y_continuous(name = "Ten year Return", limit = c(-11, 20),
+                     breaks = seq(-10, 20, 5)) +
   scale_color_manual(name = "Series",
                      values = c("Robust Regression" = "blue",
                                 "OLS Regression" = "red")) + 
@@ -118,7 +132,41 @@ my_rlm2 <- rlm(lead_10yr_ret ~ tr_cape
               ,data = cape_yearly)
 summary(my_rlm2)
 
-cape_yearly$pred_return_rlm2 <- predict(my_rlm2, newdata = cape_yearly)
+# Time Series view
+
+cape_yearly <- cape_yearly2
+complete_df <- cape_yearly[complete.cases(cape_yearly), ]
+complete_df$log_tr_cape <- log(complete_df$tr_cape)
+library(forecast)
+
+library(nlme)
+my_gls <- gls(lead_10yr_ret ~ log_tr_cape,# + I((snap_jan_yr - 1950) / 10),
+              correlation = corARMA(p = 1, q = 0),
+              data = complete_df)
+summary(my_gls)
+
+
+complete_df$pred_return_gls <- predict(my_gls, newdata = complete_df)
+ggplot(complete_df, aes(x = log_tr_cape, y = lead_10yr_ret)) +
+  #geom_point() + keep alive just to calibrate it with hjust, vjust
+  geom_text(aes(label = display_year), hjust = .5, vjust = .35, size = 2.5) +
+  geom_line(aes(y = pred_return_gls, color = "GLS Regression")) + 
+ # geom_line(aes(y = pred_return_lm, color = "OLS Regression")) + 
+  scale_x_continuous(name = "log TR-CAPE", limit = c(1.8, 4.5),
+                     breaks = seq(2, 4, 1)) +
+  scale_y_continuous(name = "Ten year Return", limit = c(-5, 20),
+                     breaks = seq(-5, 20, 5)) +
+  scale_color_manual(name = "Series",
+                     values = c("GLS Regression" = "blue",
+                                "OLS Regression" = "red")) + 
+  ggtitle("Ten year return of S&P against TR-CAPE month before investing")
+
+
+
+
+
+
+# I used this to plot the line moving up and down with time.
 ggplot(cape_yearly, aes(x = tr_cape, y = lead_10yr_ret)) +
   #geom_point() + keep alive just to calibrate it with hjust, vjust
   geom_text(aes(label = display_year), hjust = .5, vjust = .35, size = 2.5) +
@@ -160,12 +208,4 @@ ggplot(cape_yearly, aes(x = snap_jan_yr)) +
                      breaks = seq(-15, 20, 5)) +
   ggtitle("S&P500 returns: Actual (text) and predicted (blue) via TR-CAPE Regression")
 
-complete_df <- cape_yearly[complete.cases(cape_yearly), ]
-spectrum(complete_df$return_resid)
-
-library(forecast)
-my_arima <- auto.arima(complete_df$return_resid)
-complete_df$ar_pred_resid <- complete_df$return_resid - fitted(my_arima)
-
-plot(complete_df$ar_pred_resid, type = "b")
 
