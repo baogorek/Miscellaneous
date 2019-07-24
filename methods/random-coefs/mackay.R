2cec64aa621e88485dc4362424b33e172a8b92c9
Ben Ogorek <baogorek@gmail.com>
2019-07-24 08:34:12 -0400

    Up to linear regression with sleepstudy data

---
+++ methods/random-coefs/mackay.R
@@ -0,0 +1,129 @@
+library(lme4)
+write.csv(sleepstudy, "/mnt/c/devl/data/sleepstudy.csv", row.names=F)
+
+summary(lm(Reaction ~ Days, data = sleepstudy))
+
+fm1 <- lmer(Reaction ~ 1 + Days + (1 + Days | Subject), sleepstudy)
+summary(fm1)
+
+
+n_subjects <- length(table(sleepstudy$Subject))
+subject_ids <- names(table(sleepstudy$Subject))
+
+# New approach matching random effects notation
+
+# guesses / starting values
+#mu_bar <- 200
+sigma_mu <- 10
+#beta_bar <- 5
+sigma_beta <- 3
+sigma_e <- 20
+
+mu_re <- rnorm(n_subjects, mean=0, sd=sigma_mu)
+beta_re <- rnorm(n_subjects, mean=0, sd=sigma_beta)
+# end guesses / starting values
+
+X <- model.matrix(~ Days, data = sleepstudy)
+Z <- model.matrix(~ 0 + Subject + Subject:Days, data = sleepstudy)
+y <- sleepstudy$Reaction
+
+get_pred <- function(mu_bar, beta_bar, mu_re, beta_re) {
+  # X, Z are data set specific and in the global environment
+  X %*% c(mu_bar, beta_bar) + Z %*% c(mu_re, beta_re)
+}
+
+# ERROR: you're mixing up the level of mu and beta with the random effects
+# TODO: paper and pensil on this one
+neg_log_posterior <- function(mu_bar, beta_bar, mu_re, beta_re) {
+    # X, Z, y are data set specific and in the global environment
+    ss_mu <- sum(mu_re ^ 2)  # Variance of the 
+    ss_beta <- sum((beta_re - beta_bar) ^ 2)
+    # Need to think about denomonators
+    sse <- sum((get_pred(mu_bar, beta_bar, mu_re, beta_re) - y) ^ 2)
+    neg_log_post <- sse + (sigma_e ^ 2 / sigma_mu ^ 2) * ss_mu +
+      (sigma_e ^ 2 / sigma_beta ^ 2) * ss_beta
+    sse 
+}
+
+# Problem here: bad optimization
+my_optim <- optim(c(200, 10, mu_re, beta_re),
+                  function(theta){
+                    neg_log_posterior(theta[1], theta[2], theta[3:20], theta[21:38])
+                  }, method = "BFGS",
+                   control = list(maxit = 10000))
+  
+theta <- my_optim$par
+print(theta)
+
+neg_log_posterior(1, 4, mu_re, beta_re)
+
+########################################################################
+# First approach below, where random coefficients came out "fully baked"
+# This works, though it is not as general as I would like it to be
+# Step 1, get a prediction function with weights as the arguments
+
+# starting values for random effects generating parameters
+mu_bar <- 200
+sigma_mu <- 10
+beta_bar <- 5
+sigma_beta <- 3
+sigma_e <- 20
+
+# Under this representation, there's not distinction
+# between fixed and random effects
+
+theta <- c(rnorm(n_subjects, mean=mu_bar, sd=sigma_mu), # intercepts
+           rnorm(n_subjects, mean=beta_bar, sd=sigma_beta)) # slopes
+
+f <- function(theta, days, subject) {
+  subject_pos <- which(subject_ids == subject) 
+  mu_i <- theta[subject_pos]
+  beta_i <- theta[subject_pos + length(theta) / 2]
+  mu_i + beta_i * days
+}
+
+f(theta, 5, 308)
+
+get_sse <- function(theta) {
+  resid <- sapply(1:nrow(sleepstudy),
+             function(i) {
+               sleepstudy$Reaction[i] - f(theta, sleepstudy$Days[i], sleepstudy$Subject[i])
+            }
+  )
+  sum(resid ^ 2)
+}
+get_sse(theta)
+
+for (iter in 1:10) {
+  cat("Iteration ", iter, "---------------\n")
+  neg_log_posterior <- function(theta) {
+    mu <- theta[1:n_subjects]
+    beta <- theta[(n_subjects + 1):(2 * n_subjects)]
+    
+    ss_mu <- sum((mu - mu_bar) ^ 2)
+    ss_beta <- sum((beta - beta_bar) ^ 2)
+    
+    get_sse(theta) + (sigma_e ^ 2 / sigma_mu ^ 2) * ss_mu +
+      (sigma_e ^ 2 / sigma_beta ^ 2) * ss_beta
+  }
+  
+  my_optim <- optim(theta, neg_log_posterior, method = "BFGS",
+                    control = list(maxit = 10000))
+  
+  theta <- my_optim$par
+  print(theta)
+  mu <- my_optim$par[1:n_subjects]
+  beta <- my_optim$par[(n_subjects + 1):(2 * n_subjects)]
+   
+  mu_bar <- mean(mu) 
+  sigma_mu <- sd(mu)
+  beta_bar <- mean(beta) 
+  sigma_beta <- sd(beta) 
+  sigma_e <- sqrt(get_sse(my_optim$par)) / (n_subjects - 3) # is there a better effective degrees of freedom?
+}
+
+mu_bar
+sigma_mu
+beta_bar
+sigma_beta
+sigma_e
--- python-code/tensorflow/sleepstudy.py
+++ python-code/tensorflow/sleepstudy.py
@@ -4,138 +4,47 @@ import numpy as np
 import patsy
 
 sleepstudy_df = pd.read_csv("/mnt/c/devl/data/sleepstudy.csv")
-sleepstudy_df['Subject'] = pd.Categorical(sleepstudy_df['Subject'], ordered=False) 
-sleepstudy_df['one'] = 1.
-ones = sleepstudy_df.one 
-y = sleepstudy_df.Reaction
+sleepstudy_df['SubjectId'] = pd.Categorical(sleepstudy_df['Subject']).codes
+sleepstudy_df['SubjectId'] = sleepstudy_df.SubjectId.apply(lambda x: str(x))
 
-# I'd like to do a regression with Gradient tape
+X = patsy.dmatrix('1 + Days', data=sleepstudy_df)
 
-X_np = patsy.dmatrix('1 + Days', data=sleepstudy_df) #, return_type='dataframe')
-X_tf = tf.constant(X_np, dtype=tf.float32)
-y_tf = tf.constant(y.values.reshape((180, 1)), dtype=tf.float32)
-beta = tf.Variable(tf.zeros([2., 1.]), dtype=tf.float32)
-beta.assign([[200], [8]])
+Z_int = patsy.dmatrix('0 + SubjectId', data=sleepstudy_df)
+Z_slope = patsy.dmatrix('0 + SubjectId:Days', data=sleepstudy_df)
+Z = np.concatenate((Z_int, Z_slope), axis=1)
 
-@tf.function
-def get_loss(predicted_y, desired_y):
-    return tf.reduce_mean(tf.square(predicted_y - desired_y))
-
-@tf.function
-def predict(X_input, beta):
-    return tf.matmul(X_input, beta)
-
-for epoch in range(100):
-
-    with tf.GradientTape() as tape:
-        y_pred = predict(X_tf, beta) 
-        loss = get_loss(y_pred, y_tf)
-    
-    grads = tape.gradient(loss, beta)
-    
-    beta.assign_sub(.01 * grads)
-
-    print('loss is: {}'.format(loss.numpy()))
-    print('beta0: {}, beta1: {}'.format(beta.numpy()[0], beta.numpy()[1]))
-
-
-# THIS IS THE ONE!! 
-https://www.tensorflow.org/beta/guide/variables
-
-class LinReg(tf.Module):
+# Define your coefficients by subclassing from tf.Module
+class SleepReg(tf.Module):
     def __init__(self):
-        self.beta = tf.Variable(tf.zeros([2., 1.]), dtype=tf.float32)
+        self.fixed_effects = tf.Variable(tf.zeros([2, 1], dtype=tf.float64))
+        self.random_effects = tf.Variable(tf.zeros([36, 1], dtype=tf.float64))
+sleep_reg = SleepReg()
+sgd_optimizer = tf.optimizers.SGD(learning_rate=.01, momentum=.98)
 
-linreg = LinReg()
-sgd_optimizer = tf.optimizers.SGD(.01)
+@tf.function
+def predict(X, Z, beta_bar, b):
+    return tf.matmul(X, beta_bar) + tf.matmul(Z, b)
 
-for epoch in range(100):
+@tf.function
+def get_loss(observed, predicted):
+        return tf.reduce_mean(tf.square(observed - predicted))
 
-    with tf.GradientTape() as tape:
-        y_pred = predict(X_tf, linreg.trainable_variables) 
-        loss = get_loss(y_pred, y_tf)
-    
-    grads = tape.gradient(loss, linreg.trainable_variables)
-    sgd_optimizer.apply_gradients(zip(grads, linreg.trainable_variables))
+sgd_optimizer = tf.optimizers.SGD(learning_rate=.01, momentum=.98)
 
-    print('loss is: {}'.format(loss.numpy()))
-    print('beta0: {}, beta1: {}'.format(beta.numpy()[0], beta.numpy()[1]))
-
-
-# Custom Training: basics
-https://www.tensorflow.org/beta/tutorials/eager/custom_training
-# tf.keras is a neural networks API, strongly recommended for nets
-import tensorflow as tf
-# Tensors in tf are immutable stateless objects
-x = tf.zeros([10, 10]) # 10 x 10 tensor
-# But Python is stateful. Using python state:
-x += 2
-print(x)
-
-# Tensorflow has its own stateful operations built in,  like tf variables.
-# A tf Variable is an object which stores a value , implicitly reads it
-v = tf.Variable(1.0)
-v.assign(3.0)
-
-v.assign(tf.square(v))
-
-# synthesize regression data
-TRUE_W = 3.0
-TRUE_b = 2.0
-
-NUM_EXAMPLES = 100
-
-inputs  = tf.random.normal(shape=[NUM_EXAMPLES])
-noise   = tf.random.normal(shape=[NUM_EXAMPLES])
-outputs = inputs * TRUE_W + TRUE_b + noise
-
-def loss(predicted_y, desired_y):
-    return tf.reduce_mean(tf.square(predicted_y - desired_y))
-
-W = tf.Variable(5.0)
-b = tf.Variable(0.0)
-
-pred = W * inputs + b
-print(loss(pred, outputs))
-
+X = tf.constant(X)
+Z = tf.constant(Z)
+y = tf.constant(sleepstudy_df.Reaction.values.reshape((180, 1)))
 
 for epoch in range(1000):
-    with tf.GradientTape() as tape:
-        current_loss = loss(W * inputs + b, outputs)
-    dW, db = tape.gradient(current_loss, [W, b])
-    W.assign_sub(.01 * dW)
-    b.assign_sub(.01 * db)
-    print('W: {}, b: {}'.format(W.numpy(), b.numpy()))
+    with tf.GradientTape() as gradient_tape:
+        y_pred = predict(X, Z, sleep_reg.trainable_variables[0],
+                         sleep_reg.trainable_variables[1])
+        loss = get_loss(y, y_pred)
+
+    gradient = gradient_tape.gradient(loss, (sleep_reg.trainable_variables[0],
+                                             sleep_reg.trainable_variables[1]))
+    sgd_optimizer.apply_gradients(zip(gradient,
+                                      sleep_reg.trainable_variables))
+    print(sleep_reg.trainable_variables[0])
 
 
-@tf.function
-def custom_loss(obs, pred):
-    return 2 * tf.metrics.mse(obs, pred)
-
-
-# Simple Linear Regression
-one_inputs = tf.keras.Input(shape=(1,))
-x_inputs = tf.keras.Input(shape=(1,))
-
-concat_layer = tf.keras.layers.concatenate([one_inputs, x_inputs])
-
-out_layer = tf.keras.layers.Dense(1, use_bias=False)(concat_layer)
-
-model = tf.keras.Model(inputs=[one_inputs, x_inputs], outputs=out_layer,
-                       name = 'slr')
-
-my_sgd = SGD(lr = .01, decay = .01, momentum = .9)
-model.compile(optimizer=my_sgd, loss = custom_loss, metrics=['mse'])
-
-model.fit(x=[ones, sleepstudy_df.Days], y=y, epochs=1000)
-
-model.get_weights()
-
-# Now do a Regression with one beta for every subject
-my_one_hot = tf.one_hot(sleepstudy_df.Subject, depth=18)
-
-Z = patsy.dmatrix('0 + Subject', data=sleepstudy_df, return_type='dataframe')
-Z_slope = patsy.dmatrix('0 + Subject:Days', data=sleepstudy_df,
-                        return_type='dataframe')
-Z_slope = patsy.dmatrix('0 + Subject + Subject:Days', data=sleepstudy_df,
-                        return_type='dataframe')
