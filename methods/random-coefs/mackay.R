9f8420ff78b077633b88cc8d270721f234830884
Ben Ogorek <baogorek@gmail.com>
2019-08-15 22:56:05 -0700

    mackay updates

--- methods/random-coefs/mackay.R
+++ methods/random-coefs/mackay.R
@@ -1,31 +1,36 @@
 library(lme4)
-write.csv(sleepstudy, "/mnt/c/devl/data/sleepstudy.csv", row.names=F)
+#write.csv(sleepstudy, "/mnt/c/devl/data/sleepstudy.csv", row.names=F)
 
 summary(lm(Reaction ~ Days, data = sleepstudy))
 
 fm1 <- lmer(Reaction ~ 1 + Days + (1 + Days | Subject), sleepstudy)
 summary(fm1)
+# Figuring out why the variance of the random effects estimates is smaller
+# than the estimated variance
+#https://stats.stackexchange.com/questions/68106/understanding-the-variance-of-random-effects-in-lmer-models
 
 vcov(fm1) # Fixed effects variance covariance matrix
-VarCorr(fm1) # Random effects variance covariance matrix
-x = VarCorr(fm1)
-var(ranef(fm1)$Subject)
+x = VarCorr(fm1) # Random effects variance covariance matrix
+x$Subject # look at the first matrix
+var_re_hat <- var(ranef(fm1)$Subject)
 
+# In the arm package code for computing standard errors for res
+se.bygroup <- ranef(fm1, condVar=TRUE)
+n.groupings <- length(se.bygroup)
+
+vars.m <- attr(se.bygroup[[1]], "postVar") # where condVar=True is needed
+
+# all subject's random effects variances are the same in this case,
+# so the mean is just any individual variance matrix
+
+mean_V <- vars.m[,,1]
+
+var_re_hat + mean_V
+
+## Start of Mackay's implementation in R
+library(lme4)
 n_subjects <- length(table(sleepstudy$Subject))
 subject_ids <- names(table(sleepstudy$Subject))
-
-# New approach matching random effects notation
-
-# guesses / starting values
-#mu_bar <- 200
-sigma_mu <- 10
-#beta_bar <- 5
-sigma_beta <- 3
-sigma_e <- 20
-
-mu_re <- rnorm(n_subjects, mean=0, sd=sigma_mu)
-beta_re <- rnorm(n_subjects, mean=0, sd=sigma_beta)
-# end guesses / starting values
 
 X <- model.matrix(~ Days, data = sleepstudy)
 Z <- model.matrix(~ 0 + Subject + Subject:Days, data = sleepstudy)
@@ -36,37 +41,46 @@ get_pred <- function(mu_bar, beta_bar, mu_re, beta_re) {
   X %*% c(mu_bar, beta_bar) + Z %*% c(mu_re, beta_re)
 }
 
-# ERROR: you're mixing up the level of mu and beta with the random effects
-# TODO: paper and pensil on this one
 neg_log_posterior <- function(mu_bar, beta_bar, mu_re, beta_re) {
     # X, Z, y are data set specific and in the global environment
-    ss_mu <- sum(mu_re ^ 2)  # Variance of the 
-    ss_beta <- sum((beta_re - beta_bar) ^ 2)
-    # Need to think about denomonators
     sse <- sum((get_pred(mu_bar, beta_bar, mu_re, beta_re) - y) ^ 2)
-    neg_log_post <- sse + (sigma_e ^ 2 / sigma_mu ^ 2) * ss_mu +
-      (sigma_e ^ 2 / sigma_beta ^ 2) * ss_beta
-    sse 
+    (sse
+      + (sigma_e ^ 2 / sigma_mu ^ 2) * sum(mu_re ^ 2)
+      + (sigma_e ^ 2 / sigma_beta ^ 2) * sum(beta_re ^ 2)
+    )
 }
 
-# Problem here: bad optimization
-my_optim <- optim(c(200, 10, mu_re, beta_re),
-                  function(theta){
-                    neg_log_posterior(theta[1], theta[2], theta[3:20], theta[21:38])
-                  }, method = "BFGS",
-                   control = list(maxit = 10000))
-  
-theta <- my_optim$par
-print(theta)
+# Starting values
+mu_re <- rep(0, n_subjects) # rnorm(n_subjects, mean=0, sd=sigma_mu)
+beta_re <- rep(0, n_subjects) #rnorm(n_subjects, mean=0, sd=sigma_beta)
+theta_starting <- c(200, 10, mu_re, beta_re)
 
-neg_log_posterior(1, 4, mu_re, beta_re)
+# guesses / starting values
+sigma_mu <- 10
+sigma_beta <- 3
+sigma_e <- 20
+
+# Manually loop below
+for(rep in 1:100) {
+  my_optim <- optim(theta_starting,
+                    function(theta){
+                      neg_log_posterior(theta[1], theta[2], theta[3:20], theta[21:38])
+                    }, method = "BFGS",
+                     control = list(maxit = 10000))
+    
+  theta <- my_optim$par
+  sigma_mu <- sd(theta[3:20])
+  sigma_beta <- sd(theta[21:38])
+  resid <- get_pred(theta[1], theta[2], theta[3:20], theta[21:38]) - y
+  sigma_e <- sqrt(sum(resid ^ 2) / (length(resid) - 3.8))
+  
+  cat('sigma_mu: ', sigma_mu, ', sigma_beta: ', sigma_beta,
+      'sigma_e: ', sigma_e, '\n')
+}
+theta
 
 ########################################################################
 # First approach below, where random coefficients came out "fully baked"
-# This works, though it is not as general as I would like it to be
-# Step 1, get a prediction function with weights as the arguments
-
-# starting values for random effects generating parameters
 mu_bar <- 200
 sigma_mu <- 10
 beta_bar <- 5
--- python-code/tensorflow/mackay.py
+++ python-code/tensorflow/mackay.py
@@ -1,20 +1,50 @@
+load_ext autoreload
+autoreload 2
+
+# Part 1. Demo of lmer matching
 from sleepstudy import SleepReg
 import numpy as np
 
 sleep_reg = SleepReg("/mnt/c/devl/data/sleepstudy.csv")
+
+sleep_reg.train(mackay=True)
+
+off_diag = 24.7404 * 5.9221 * 0.066
+sleep_reg.reset_variances(np.array([[24.7404 ** 2, off_diag],
+                                    [off_diag, 5.9221 ** 2]]),
+                          25.5918 ** 2)
+
+sleep_reg.train(mackay=True)
+
 sleep_reg.reset_variances(np.array([[410, 10], [10, 22]]),
                          .25 * np.var(sleep_reg.y))
 
-for i in range(5):
-    sleep_reg.train(display_beta=False)
+off_diag = 0.0 
+sleep_reg.reset_variances(np.array([[24.7404 ** 2, off_diag],
+                                    [off_diag, 5.9221 ** 2]]),
+                          25.5918 ** 2)
+
+off_diag = 0.0 
+sleep_reg.reset_variances(np.array([[44.7404 ** 2, off_diag],
+                                    [off_diag, 9.9221 ** 2]]),
+                          35.5918 ** 2)
+
+for i in range(100):
+    sleep_reg.train(display_beta=False, mackay=False)
     
     Sigma_b = sleep_reg.estimate_Sigma_b()
     sigmasq_epsilon = sleep_reg.estimate_sigmasq_epsilon()
-    
+   
+    Sigma_b[0, 1] = 0.0
+    Sigma_b[1, 0] = 0.0
     sleep_reg.reset_variances(Sigma_b, sigmasq_epsilon)
+
     print(sigmasq_epsilon)
     print(Sigma_b)
 
+#[670.8524944]
+#[[302.92110182   0.        ]
+#[  0.          31.08864589]]
 #sleep_reg.set_optimizer(adam=True)
 #sleep_reg.train()
 
