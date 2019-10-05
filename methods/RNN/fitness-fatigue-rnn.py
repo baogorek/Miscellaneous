63def5828615a7298c681643f3e67fac021e8647
Ben Ogorek <baogorek@gmail.com>
2019-10-05 17:34:13 -0400

    some scaling. Still off on the predictions

--- methods/RNN/fitness-fatigue-rnn.py
+++ methods/RNN/fitness-fatigue-rnn.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
-
+from matplotlib import pyplot as plt
 import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.layers import GRU, LSTM, Dense, Activation, Input
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.regularizers import l2
@@ -12,19 +13,46 @@ from tensorflow.keras.regularizers import l2
 train_df = pd.read_csv('train_df.csv')
 train_df['perf_lag1'] = train_df.perf.shift(1)
 
-train_df = train_df.iloc[1:, :] # don't leave that NA from the lagging in there
+val_df = pd.read_csv('val_df.csv')
+val_df['perf_lag1'] = val_df.perf.shift(1)
 
-X = np.reshape(train_df[['perf_lag1', 'w']].values, (258, 1, 2)) 
-y = np.reshape(train_df['perf'].values, (258, )) 
+
+train_df = train_df.iloc[1:, :] # don't leave that NA from the lagging in there
+val_df = val_df.iloc[1:, :] # don't leave that NA from the lagging in there
+
+scaler = StandardScaler()
+train_inputs = scaler.fit_transform(train_df[['perf_lag1', 'w']].values)
+# TODO: Understand the scaler
+val_inputs = scaler.fit_transform(val_df[['perf_lag1', 'w']].values)
+
+X_train = np.reshape(train_inputs, (258, 1, 2)) 
+y_train = np.reshape(train_df['perf'].values, (258, )) / 250.
+
+X_val = np.reshape(val_inputs, (258, 1, 2)) 
+y_val = np.reshape(val_df['perf'].values, (258, )) / 250.
 
 rnn = Sequential()
-#rnn.add(GRU(10, stateful=True, batch_input_shape=(1, 1, 2)))
-rnn.add(GRU(10, stateful=True, input_shape=(1, 2), batch_size = 1,
-            kernel_regularizer=l2(.5)))
+rnn.add(GRU(100, stateful=True, input_shape=(1, 2), batch_size = 1))
+            #kernel_regularizer=l2(.5)))
 rnn.add(Dense(1, activation='linear', kernel_regularizer=l2(.5)))
 rnn.compile(optimizer='adam', loss='mse')
 
-for epochs in range(1000):
-    rnn.fit(X, y, epochs=1,  batch_size=1, shuffle=False)
+for epoch in range(50):
     rnn.reset_states()
+    rnn.fit(X_train, y_train, epochs=1,  batch_size=1, shuffle=False)
 
+    rnn.reset_states()
+    yhat_train = rnn.predict(X_train).flatten()
+    mae_train = np.mean(np.abs(yhat_train - y_train))
+
+    rnn.reset_states()
+    yhat_val = rnn.predict(X_val).flatten()
+    mae_val = np.mean(np.abs(yhat_val - y_val))
+    print(f'epoch {epoch}, train MAE: {mae_train:.2f}, val MAE: {mae_val:.2f}')
+
+plt.plot(yhat_train * 250)
+plt.plot(yhat_val * 250)
+
+np.corrcoef(yhat_train, y_train)
+np.corrcoef(yhat_val, y_val)
+np.corrcoef(yhat_val, y_val)
