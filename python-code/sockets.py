131bd54b24e9db26e36249e7d996c0eb7a2a6868
Benjamin Ogorek <baogorek@gmail.com>
2019-11-05 15:44:38 -0500

    Update sockets.py
    Updating to work in Python3

--- python-code/sockets.py
+++ python-code/sockets.py
@@ -2,19 +2,16 @@
 # Shell 1 will be our server, Shell 2 will be our client
 
 ##### In Shell 1 #####
-
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("127.0.0.1", 5000))
 server_socket.listen(5) # Max 5 connect requests in queue
 
-# It's strange that the sole purpose of server sockets is to create client sockets, on the server
 # Shell 1 will seem to hang after running this line
-(client_socket_on_server, address) = server_socket.accept()
+client_socket_on_server, address = server_socket.accept()
 
 ##### In Shell 2 #####
-
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,7 +19,7 @@ client_socket.connect(("127.0.0.1", 5000))
 
 ##### In Shell 1 #####
 # Which is now back in your control
-client_socket_on_server.send("hey")
+client_socket_on_server.send("hey".encode()) # in Python3, default strings are unicode
 
 ##### In Shell 2 #####
-client_socket.recv(3) # three characters in 'hey'
+client_socket.recv(100) # three characters in 'hey', 100 char buffer
