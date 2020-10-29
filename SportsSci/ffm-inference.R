92da7d4635f0f1b8cdcad58fdf8fa1c6ba22adde
baogorek <baogorek@github.com>
2020-10-29 09:07:43 -0500

    Rename KF quantities. Add simulation via KF

--- SportsSci/ffm-inference.R
+++ SportsSci/ffm-inference.R
@@ -162,7 +162,6 @@ points(perf_hat ~ t, data = training_df, type = 'b', col = 'blue')
 
 # Fitting with Kalman Filter ------------------------------------------------
 
-
 training_df <- read.csv('training_df.csv')
 
 # Modify to fix state intercept: initial conditions:
@@ -176,7 +175,10 @@ if (FALSE) {
   k_h <- .15
   tau_g <- 20
   tau_h <- 5 
-  sigma_e <- sqrt(35) 
+  xi <- sqrt(35) 
+  sigma_g <- 0
+  sigma_h <- 0
+  sigma_gh <- 0
   prior_mean_fitness <- 0
   prior_mean_fatigue <- 0
   prior_sd_fitness <- 1 
@@ -184,87 +186,124 @@ if (FALSE) {
   prior_ff_corr <- 0
 }
 
-do_ffm_kalman <- function(training_df, p_0, k_g, k_h, tau_g, tau_h, sigma_e,
+do_ffm_kalman <- function(training_df, p_0, k_g, k_h, tau_g, tau_h,
+			  xi, sigma_g = 0, sigma_h = 0, sigma_gh = 0,
 			  prior_mean_fitness=0, prior_mean_fatigue=0,
 			  prior_sd_fitness=25, prior_sd_fatigue=25,
 			  prior_ff_corr=0) {
 
-  # Length of the time series
+  # Data set extractions
   T <- nrow(training_df)
+  w <- training_df$w
+  y <- training_df$perf
 
   # Time-stable Kalman Filter matrices -----------------------------------------
   # Transition matrix
-  T_mat <- matrix(c(exp(-1 / tau_g), 0, 0, exp(-1 / tau_h)), ncol=2)
+  A_mat <- matrix(c(exp(-1 / tau_g), 0, 0, exp(-1 / tau_h)), ncol=2)
   
   # State intercept - each row is the fitness and fatigue effect to the next day's workout
   B_mat <- matrix(c(exp(-1 / tau_g), exp(-1 / tau_h)), ncol=1)
  
   # Measurement matrix
-  H_mat <- matrix(c(k_g, -k_h), ncol=2)
+  C_mat <- matrix(c(k_g, -k_h), ncol=2)
   
-  # Variance matrices
-  Q_mat <- matrix(c(0, 0, 0, 0), ncol=2) #  0-state error for starters
-  R_mat <- matrix(c(sigma_e ^ 2), ncol=1) # 1x1 matrix with meas.err. variance
+  # Variances
+  Q_mat <- matrix(c(sigma_g ^ 2, sigma_gh, sigma_gh, sigma_h ^ 2), ncol=2)
+  #R_mat is just xi ^ 2
  
   # prior distribution of fitness and fatigue
-  mu0 <- c(prior_mean_fitness, prior_mean_fatigue)
-  Sigma0 <- matrix(c(prior_sd_fitness ^ 2,
-                   rep(prior_ff_corr * prior_sd_fitness * prior_sd_fatigue, 2),
-                   prior_sd_fatigue ^ 2), ncol=2)
+  x0 <- c(prior_mean_fitness, prior_mean_fatigue)
+  M0 <- matrix(c(prior_sd_fitness ^ 2,
+                 rep(prior_ff_corr * prior_sd_fitness * prior_sd_fatigue, 2),
+                 prior_sd_fatigue ^ 2), ncol=2)
   
-  # Setting up structures to be predicted ----------------------------------
+  # Setting up structures ----------------------------------
   log_likelihood <- numeric(T)
-  state_intercept <- matrix(rep(NA, 2 * T), ncol=2)
-  mu <- matrix(rep(NA, 2 * T), ncol=2)  # I.e., fitness and fatigue
-  Sigma <- matrix(rep(NA, 4 * T), ncol=4) # Rows contain vectorized Sigma_t
+  X <- matrix(rep(NA, 2 * T), ncol=2)  # State matrix containing fitness and fatigue
+  M <- matrix(rep(NA, 4 * T), ncol=4) # Rows contain vectorized cov matrices
 
-  # State Prior to Likelihood to State Posterior - the Kalman updating equations
-  for (t in 1:T) { # 4 is where the problem is
-    if (t == 1) { 
-      # Prior (a priori) mean and variance of state
-      systematic_t <- mu0
-      P_t <- Sigma0
-      state_intercept[1, ] <-  as.numeric(B_mat * training_df$w[t])  # affects t = 2
+  # The Kalman updating equations
+  for (n in 1:T) {
+
+    # A priori mean and variance of state -- 
+    if (n == 1) { 
+      z_n <- x0
+      P_n <- M0
     } else {
-      systematic_t <- T_mat %*% mu[t - 1, ] + as.matrix(state_intercept[t - 1, ], ncol = 1)
-      P_t <- T_mat %*% matrix(Sigma[t - 1, ], ncol=2) %*% t(T_mat) + Q_mat
-      state_intercept[t, ] <- as.numeric(B_mat * training_df$w[t])  # affects t + 1
+      z_n <- A_mat %*% X[n - 1, ] + B_mat * w[n - 1]
+      P_n <- Q_mat + A_mat %*% matrix(M[n - 1, ], ncol=2) %*% t(A_mat)
     }
  
-    # Likelihood of performance given systematic state update
-    y_t <- training_df$perf[t]
-    S_t <- H_mat %*% P_t %*% t(H_mat) + R_mat
-    e_t <- y_t - (p_0 + H_mat %*% systematic_t)
-    log_likelihood[t] <- dnorm(e_t, mean = 0, sd = sqrt(S_t), log=TRUE)
+    # Likelihood of perf measurement n --
+    S_n <- xi ^ 2 + C_mat %*% P_n %*% t(C_mat)  # pre-fit residual covariance
+    e_n <- y[n] - (p_0 + C_mat %*% z_n)
+    log_likelihood[n] <- dnorm(e_n, mean = 0, sd = sqrt(S_n), log=TRUE)
   
-    # Posterior of state (fitness and fatigue) given all data up to time t
-    S_t_inv <- 1 / S_t  #  univariate inverse
-    mu[t, ] <- systematic_t + P_t %*% t(H_mat) %*% S_t_inv %*% e_t  # wrong at t = 4, right at t=1,2,3
-    Sigma[t, ] <- as.vector(P_t - P_t %*% t(H_mat) %*% S_t_inv %*% H_mat %*% P_t)  # Correct at t=4
+    # A posteriori mean and variance of state --
+    K_n <- P_n %*% t(C_mat) %*% (1 / S_n)  # Kalman Gain
+    X[n, ] <- z_n + K_n %*% e_n
+    M[n, ] <- as.vector((diag(2) - K_n %*% C_mat) %*% P_n)
   }
   
-  perf_hat <- p_0 + mu %*% t(H_mat)  # Filtered predictions of performance
+  perf_hat <- p_0 + X %*% t(C_mat)  # Filtered predictions of performance
 
-  list(perf_hat=perf_hat, mu=mu, Sigma=Sigma, log_likelihood=log_likelihood)
+  list(perf_hat=perf_hat, mu=X, Sigma=M, log_likelihood=log_likelihood)
 }
 
+# Going for generation
+  # prior distribution of fitness and fatigue
+  x0 <- c(prior_mean_fitness, prior_mean_fatigue)
+  M0 <- matrix(c(prior_sd_fitness ^ 2,
+                 rep(prior_ff_corr * prior_sd_fitness * prior_sd_fatigue, 2),
+                 prior_sd_fatigue ^ 2), ncol=2)
+  
+  # Setting up structures ----------------------------------
+  y <- numeric(T)
+  X <- matrix(rep(NA, 2 * T), ncol=2)  # State matrix containing fitness and fatigue
+  M <- matrix(rep(NA, 4 * T), ncol=4) # Rows contain vectorized Sigma_t
+
+  # State Prior to Likelihood to State Posterior - the Kalman updating equations
+  for (n in 1:T) {
+
+    # A priori mean and variance of state -- 
+    if (n == 1) { 
+      z_n <- x0
+      P_n <- M0
+    } else {
+      z_n <- A_mat %*% X[n - 1, ] + B_mat * w[n - 1]
+      P_n <- Q_mat + A_mat %*% matrix(M[n - 1, ], ncol=2) %*% t(A_mat)
+    }
+    M[n, ] <- as.numeric(P_n)
+ 
+    # Likelihood of perf measurement n --
+    S_n <- xi ^ 2 + C_mat %*% P_n %*% t(C_mat)  # pre-fit residual covariance
+    y[n] <- rnorm(1, mean = p_0 + C_mat %*% z_n, sd = sqrt(S_n))
+    X[n, ] <- z_n 
+  }
+ 
+sim_df <- data.frame(w=w, perf=y)  #TODO: response has to be called "perf" - make robust
+
+
+do_ffm_kalman <- function(training_df, p_0, k_g, k_h, tau_g, tau_h,
+			  xi, sigma_g = 0, sigma_h = 0, sigma_gh = 0,
+			  prior_mean_fitness=0, prior_mean_fatigue=0,
+			  prior_sd_fitness=25, prior_sd_fatigue=25,
+			  prior_ff_corr=0) {
+
+
 # For matching Python with starting values:
-results <- do_ffm_kalman(training_df, 400, .05, .15, 20, 5, sqrt(35),
-			  prior_mean_fitness=0, prior_mean_fatigue=0,
-			  prior_sd_fitness=1, prior_sd_fatigue=1,
-			  prior_ff_corr=0)
-sum(results$log_likelihood)K
-results <- do_ffm_kalman(training_df, 495.1478, .0797, .2679, 56.3461, 13.9621, sqrt(115.1329),
-			  prior_mean_fitness=0, prior_mean_fatigue=0,
-			  prior_sd_fitness=1, prior_sd_fatigue=1,
-			  prior_ff_corr=0)
-sum(results$log_likelihood)
+results <- do_ffm_kalman(sim_df, 400, .05, .15, 20, 5, sqrt(35),
+			 sigma_g = 0, sigma_h = 0, sigma_gh = 0,
+			 prior_mean_fitness=0, prior_mean_fatigue=0,
+			 prior_sd_fitness=1, prior_sd_fatigue=1,
+			 prior_ff_corr=0)
 
-
-fit_ffm_via_kalman <- function(starting_theta,
-			       prior_mean_fitness=0, prior_mean_fatigue=0,
-			       prior_sd_fitness=1, prior_sd_fatigue=1,
-			       prior_ff_corr=0) {
+# Now for fitting
+fit_ffm_via_kalman <- function(df, starting_theta,
+			       sigma_g = 0, sigma_h = 0, sigma_gh = 0,
+			       prior_mean_fitness = 0, prior_mean_fatigue = 0,
+			       prior_sd_fitness = 1, prior_sd_fatigue = 1,
+			       prior_ff_corr = 0) {
 
   get_negloglike <- function(theta) {
     p_0 <- theta[1] # performance baseline
@@ -272,10 +311,10 @@ fit_ffm_via_kalman <- function(starting_theta,
     k_h <- theta[3] # fatigue weight
     tau_g <- theta[4] # fitness decay
     tau_h <- theta[5] # fatigue decay
-    sigma_e <- theta[6] # standard deviation of error
+    xi <- theta[6] # standard deviation of error
   
-    filtered <- do_ffm_kalman(training_df, p_0, k_g, k_h, tau_g, tau_h,
-			      sigma_e,
+    filtered <- do_ffm_kalman(df, p_0, k_g, k_h, tau_g, tau_h, xi,
+			      sigma_g, sigma_h, sigma_gh,
 			      prior_mean_fitness, prior_mean_fatigue,
 			      prior_sd_fitness, prior_sd_fatigue,
 			      prior_ff_corr)
@@ -287,13 +326,13 @@ fit_ffm_via_kalman <- function(starting_theta,
 }
 
 # Fitting the FFM using the functions above ----------------------
-starting_theta <- c(400, .05, .15, 20, 5, sqrt(35))
-ffm_optim <- fit_ffm_via_kalman(starting_theta,
+starting_theta <- c(400, .08, .25, 50, 15, sqrt(35))
+ffm_optim <- fit_ffm_via_kalman(sim_df, starting_theta,
 		                prior_mean_fitness=0, prior_mean_fatigue=0,
 		                prior_sd_fitness=1, prior_sd_fatigue=1,
 		                prior_ff_corr=0)
-theta_hat <- ffm_optim$par
-print(theta_hat)
+ffm_optim
+(theta_hat <- ffm_optim$par)
 
 
 ffm_fit <- do_ffm_kalman(training_df, theta_hat[1], theta_hat[2], theta_hat[3],
