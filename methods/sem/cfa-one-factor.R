bed533e46a81bbe9ca2797e75c06a24c399b70c0
bogorek <bogorek@spencerhealthsolutions.com>
2023-01-07 09:20:12 -0500

    Hugh Crew's addition: centering fixes the problem

--- methods/sem/cfa-one-factor.R
+++ methods/sem/cfa-one-factor.R
@@ -1,5 +1,7 @@
 library(lavaan)
 
+set.seed(12345)
+
 options(width=300)
 
 # Set the parameters and sample size
@@ -23,28 +25,34 @@ y1 <- lambda1 * L + e_1
 y2 <- lambda2 * L + e_2
 y3 <- lambda3 * L + e_3
 
-df <- data.frame(y1=y1, y2=y2, y3=y3)
+
+y1b <- scale(y1, scale=F)
+y2b <- scale(y2, scale=F)
+y3b <- scale(y3, scale=F)
+
+
+df2 <- data.frame(y1=y1b, y2=y2b, y3=y3b)
 
 # Now recover the parameters with lavaan
 
 ## By default, lavaan fixes the first loading to 1
 model_string <- ' L =~ y1 + y2 + y3'
-my_cfa <- cfa(model_string, data = df)
+my_cfa_default <- cfa(model_string, data = df2)
 
 # Note that .y1 is actually the residual e1.
-summary(my_cfa)
-coef(my_cfa)
+summary(my_cfa_default)
+coef(my_cfa_default)
 
 # We can tell levaan to use latent variable variance = 1 constraint instead
 model_string <- ' L =~ NA*y1 + y1 + y2 + y3 '
-my_cfa <- cfa(model_string, data = df, std.lv=TRUE)
+my_cfa <- cfa(model_string, data = df2, std.lv=TRUE)
 
 summary(my_cfa)
 # The following should closely match our parameters (depending on N)
 coef(my_cfa)
 
 # get the factor scores from Lavaan
-df$L_hat <- as.numeric(predict(my_cfa))
+df2$L_hat <- as.numeric(predict(my_cfa))
 
 # Now apply the covariance formula to the factor scores
 coefs <- as.numeric(coef(my_cfa))
@@ -70,17 +78,14 @@ lambda_vec_hat <- matrix(c(lambda1_hat, lambda2_hat, lambda3_hat), nrow=1)
 hat_matrix <- lambda_vec_hat %*% solve(E_cov_partial_hat)
 
 # Match the first observiation
-hat_matrix %*% matrix(as.numeric(df[1, c("y1", "y2", "y3")]), nrow=3)
-df[1, "L_hat"]
+hat_matrix %*% matrix(as.numeric(df2[1, c("y1", "y2", "y3")]), nrow=3)
+df2[1, "L_hat"]
 
 # Match the second observation
-hat_matrix %*% matrix(as.numeric(df[2, c("y1", "y2", "y3")]), nrow=3)
-df[2, "L_hat"]
+hat_matrix %*% matrix(as.numeric(df2[2, c("y1", "y2", "y3")]), nrow=3)
+df2[2, "L_hat"]
 
 # Now do it for the whole data set
-df$L_hat2 <- as.matrix(df[, c("y1", "y2", "y3")]) %*% t(hat_matrix)
-head(df)
-tail(df)
-
-# Note: not entirely sure why the match isn't exact
-
+df2$L_hat2 <- as.matrix(df2[, c("y1", "y2", "y3")]) %*% t(hat_matrix)
+head(df2)
+tail(df2)
