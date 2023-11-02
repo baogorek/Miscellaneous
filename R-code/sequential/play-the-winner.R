52a2c04d61ab7791038a0996e7e14105aeb98897
bogorek <bogorek@spencerhealthsolutions.com>
2023-11-02 10:47:18 -0400

    play the winner simulation

--- R-code/sequential/play-the-winner.R
+++ R-code/sequential/play-the-winner.R
@@ -1,5 +1,5 @@
-rm(list = ls())
 
+## Initial exploration with randomized play the winner -------------------
 N.units <- 500 # Number of experimental units that will arrive sequentially 
 alpha <-  1 # Initial state of the urn: there are alpha balls of each type.
 
@@ -58,7 +58,143 @@ plot(contest$p.B.est ~ contest$unit.id, type = "o", cex = .3, col = "red",
 lines(rep(p.B, times = nrow(contest)) ~ contest$unit.id, type = "l", col = "green")
 
 
+# Starting over on 2023-11-01 after reading paper ------
+
+simulate_trial <- function(p_a, p_b, stop_value) {
+  # Initial counts
+  r_a <- 0
+  r_b <- 0
+  
+  # Keep track of the history
+  history_a <- c()
+  history_b <- c()
+  
+  while (r_a < stop_value && r_b < stop_value) {
+    # Calculate the current probability to draw treatment A
+    prob_a <- r_a / (r_a + r_b)
+    
+    # If r_a and r_b are both 0, we need to randomly select a treatment since prob_a will be NaN
+    if (is.nan(prob_a)) {
+      prob_a <- 0.5
+    }
+    
+    # Randomly draw a treatment
+    treatment_drawn <- ifelse(runif(1) < prob_a, "A", "B")
+    
+    if (treatment_drawn == "A") {
+      # Check for success or failure
+      if (runif(1) < p_a) {
+        # Success for treatment A
+        r_a <- r_a + 1
+      } else {
+        # Failure for treatment A
+        r_b <- r_b + 1
+      }
+    } else {
+      # Check for success or failure
+      if (runif(1) < p_b) {
+        # Success for treatment B
+        r_b <- r_b + 1
+      } else {
+        # Failure for treatment B
+        r_a <- r_a + 1
+      }
+    }
+    
+    # Append to history
+    history_a <- c(history_a, r_a)
+    history_b <- c(history_b, r_b)
+  }
+  
+  return(list(history_a = history_a, history_b = history_b))
+}
+
+# Simulation parameters
+grid <- merge(
+  data.frame(r=c(5, 10, 15, 30, 50)),
+  data.frame(p_a=c(.3, .7, .9, .5, .8, .7), p_b = c(.1, .5, .7, .1, .4, .1)),
+  all=TRUE
+)[, c(2, 3, 1)]
+
+M <- 100000  # Monte Carlo reps
+for (j in 1:nrow(grid)) {
+  p_a <- grid[j, "p_a"]
+  p_b <- grid[j, "p_b"]
+  r <- grid[j, "r"]
+  cat("running with p_a =", p_a, ", p_b =", p_b, ", r =", r, "for", M, "reps\n")
+  correct_stopping <- numeric(M)
+  sample_size <- numeric(M)
+  for (rep in 1:M) {
+    result <- simulate_trial(p_a, p_b, r)
+    correct_stopping[rep] <- max(result$history_a) == r
+    sample_size[rep] <- length(result$history_a)
+  }
+  grid[j, "pr_correct_stopping"] <- mean(correct_stopping)
+  grid[j, "average_sample_number"] <- mean(sample_size)
+}
+
+print(grid, digits=3)
 
 
+# Modified Randomized Play the Winner with no balls given when the other team loses
+simulate_trial2 <- function(p_a, p_b, stop_value) {
+  # NOTE: in this modified RPW, if you don't start with 1 ball, you'll have 0 probabilities
+  #  appearing after the first win
+  r_a <- 1
+  r_b <- 1
+  history_a <- c()
+  history_b <- c()
+  while (r_a < stop_value && r_b < stop_value) {
+    # Calculate the current probability to draw treatment A
+    prob_a <- r_a / (r_a + r_b)
+    treatment_drawn <- ifelse(runif(1) < prob_a, "A", "B")
+    if (treatment_drawn == "A") {
+      # Check for success or failure
+      if (runif(1) < p_a) {
+        # Success for treatment A
+        r_a <- r_a + 1
+      } else {
+        # Failure for treatment A - NOTHING HAPPENS
+      }
+    } else {
+      # Check for success or failure
+      if (runif(1) < p_b) {
+        # Success for treatment B
+        r_b <- r_b + 1
+      } else {
+        # Failure for treatment B - NOTHING HAPPENS
+      }
+    }
+    # Append to history
+    history_a <- c(history_a, r_a)
+    history_b <- c(history_b, r_b)
+  }
+  
+  return(list(history_a = history_a, history_b = history_b))
+}
 
+# Simulation parameters
+grid <- merge(
+  data.frame(r=c(5, 10, 15, 30, 50)),
+  data.frame(p_a=c(.3, .4, .1),
+             p_b=c(.1, .2, .05)),
+  all=TRUE
+)[, c(2, 3, 1)]
 
+M <- 10000  # Monte Carlo reps
+for (j in 1:nrow(grid)) {
+  p_a <- grid[j, "p_a"]
+  p_b <- grid[j, "p_b"]
+  r <- grid[j, "r"]
+  cat("running with p_a =", p_a, ", p_b =", p_b, ", r =", r, "for", M, "reps\n")
+  correct_stopping <- numeric(M)
+  sample_size <- numeric(M)
+  for (rep in 1:M) {
+    result <- simulate_trial2(p_a, p_b, r)
+    correct_stopping[rep] <- max(result$history_a) == r
+    sample_size[rep] <- length(result$history_a)
+  }
+  grid[j, "pr_correct_stopping"] <- mean(correct_stopping)
+  grid[j, "average_sample_number"] <- mean(sample_size)
+}
+print(grid, digits=3)
